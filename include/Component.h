5e7cb0a129018d906198225d9f7f001309ca01aa
Lucas Martins <lucas465@hotmail.com>
2019-04-14 20:56:16 -0300

    Create class Component

---
+++ include/Component.h
@@ -0,0 +1,22 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <iostream>
+
+#include "GameObject.h"
+
+using namespace std;
+
+class Component {
+protected:
+  GameObject associated;
+
+public:
+  Component(GameObject &associated);
+
+  virtual ~Component();
+  virtual void Update(float delta) = 0;
+  virtual void Render() = 0;
+  virtual bool Is(const char *type) = 0;
+};
+#endif
