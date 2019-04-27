3aff388ca428994115aecebe9a8bc92a874585ac
Lucas Martins <lucas465@hotmail.com>
2019-04-27 14:44:14 -0300

    Modifies sprite class as child of Component

--- include/Sprite.h
+++ include/Sprite.h
@@ -1,32 +1,35 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include <iostream>
-#include <string>
-
 #define INCLUDE_SDL_IMAGE
 #include "SDL_include.h"
 
+#include <iostream>
+
+#include "Component.h"
+
 using namespace std;
 
-class Sprite {
+class Sprite : public Component {
 private:
   SDL_Texture *texture;
+  SDL_Rect clipRect;
   int width;
   int height;
-  SDL_Rect clipRect;
 
 public:
-  Sprite();
-  Sprite(const char *file);
+  Sprite(GameObject &associated);
+  Sprite(GameObject &associated, const char *file);
 
   ~Sprite();
 
+  void Render();
+  void Update(float delta);
   void Open(const char *file);
   void SetClip(int x, int y, int width, int height);
-  void Render(int x, int y);
   int GetWidth();
   int GetHeight();
   bool IsOpen();
+  bool Is(const char *type);
 };
 #endif
